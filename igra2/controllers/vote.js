c353ff1acc4954fbb247b397f67730112751d351
Ivan <salvadx77@gmail.com>
2024-05-24 21:31:55 +0300

    dada

--- igra2/controllers/vote.js
+++ igra2/controllers/vote.js
@@ -1,5 +1,5 @@
 const fs = require("fs").promises;
-const parseBody = require("../appModules/http-utils/parse-body");
+const httpUtils  = require("../appModules/http-utils/parse-body");
 const { config, createRating, updateRating } = require("../appModules/rating");
 
 async function voteRouteController(req, res) {
@@ -7,9 +7,9 @@ async function voteRouteController(req, res) {
     res.statusCode = 404;
     res.end("Not Found");
   } else {
-    try {
+  
       res.statusCode = 200;
-      const body = await parseBody(req);
+      const body = await httpUtils.parseBody(req);
       const data = JSON.parse(body);
       const rating = createRating(data, config.WEIGHT);
 
@@ -20,11 +20,8 @@ async function voteRouteController(req, res) {
       await fs.writeFile(config.PATH_TO_RATING_FILE, JSON.stringify(newRating));
       res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify(newRating.sort((a, b) => b.rating - a.rating)));
-    } catch (error) {
-      res.statusCode = 500;
-      res.end("Internal Server Error");
-    }
+    } 
   }
-}
+
 
 module.exports = voteRouteController;
